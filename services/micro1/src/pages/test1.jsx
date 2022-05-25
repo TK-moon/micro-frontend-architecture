60ec5504d5c645ffea048e6deadac35399f71b16
TK-moon <tkmoon.dev@gmail.com>
2022-05-25 17:31:41 +0900

    :wrench: fix prettier and reformat files

--- services/micro1/src/pages/test1.jsx
+++ services/micro1/src/pages/test1.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react"
 
 const Test1 = () => {
-  console.log('Page1')
+  console.log("Page1")
 
   return (
     <div>
--- services/micro1/src/pages/test2.jsx
+++ services/micro1/src/pages/test2.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react"
 
 const Test2 = () => {
-  console.log('Page2')
+  console.log("Page2")
 
   return (
     <div>
