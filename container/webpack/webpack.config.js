4871a94f3e98dfe83b17fb5462194a07ed48bfa4
TK-moon <tkmoon.dev@gmail.com>
2022-05-27 00:39:42 +0900

    :wrench: add ts-checker-plugin to webpack config

--- container/webpack/webpack.config.js
+++ container/webpack/webpack.config.js
@@ -1,36 +1,44 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { ModuleFederationPlugin } = require('webpack').container;
-const { merge } = require('webpack-merge');
-const path = require('path');
-const dotenv = require('dotenv')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { ModuleFederationPlugin } = require("webpack").container
+const { merge } = require("webpack-merge")
+const path = require("path")
+const dotenv = require("dotenv")
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
 
-const webpackDevConfig = require('./webpack.config.dev')
-const webpackProdConfig = require('./webpack.config.prod')
-const webpackLocalConfig = require('./webpack.config.local')
+const webpackDevConfig = require("./webpack.config.dev")
+const webpackProdConfig = require("./webpack.config.prod")
+const webpackLocalConfig = require("./webpack.config.local")
 
 function getRemoteEntryUrl(appName) {
   switch (appName) {
-    case "micro1": return `${process.env.MFE_SERVICE_1_URL}/remoteEntry.js`
-    case "micro2": return `${process.env.MFE_SERVICE_2_URL}/remoteEntry.js`
+    case "micro1":
+      return `${process.env.MFE_SERVICE_1_URL}/remoteEntry.js`
+    case "micro2":
+      return `${process.env.MFE_SERVICE_2_URL}/remoteEntry.js`
   }
 }
 
 const getDotEnvPath = (mode) => {
-  switch(mode) {
-    case 'development': return './env/.env.development'
-    case 'production': return './env/.env.production'
-    default: return './env/.env.localhost'
+  switch (mode) {
+    case "development":
+      return "./env/.env.development"
+    case "production":
+      return "./env/.env.production"
+    default:
+      return "./env/.env.localhost"
   }
 }
 
 const getWebpackConfigByMode = (mode) => {
-  switch(mode) {
-    case 'development': return webpackDevConfig
-    case 'production': return webpackProdConfig
-    default: return webpackLocalConfig
+  switch (mode) {
+    case "development":
+      return webpackDevConfig
+    case "production":
+      return webpackProdConfig
+    default:
+      return webpackLocalConfig
   }
 }
-
 
 module.exports = (env, argv) => {
   const PRODUCTION_MODE = argv.mode // production | development | none(localhost)
@@ -39,52 +47,53 @@ module.exports = (env, argv) => {
   dotenv.config({ path: ENV_PATH })
 
   const commonConfig = {
-    entry: './src/index.ts',
-    target: 'web',
+    entry: "./src/index.ts",
+    target: "web",
     output: {
-      filename: '[name].[chunkhash].js',
-      publicPath: 'auto',
+      filename: "[name].[chunkhash].js",
+      publicPath: "auto",
       clean: true,
     },
     resolve: {
-      extensions: ['.ts', '.tsx', '.js'],
+      extensions: [".ts", ".tsx", ".js"],
       alias: {
-        '@': path.resolve(__dirname, './src')
-      }
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
     module: {
       rules: [
         {
           test: /\.tsx?$/,
-          loader: 'esbuild-loader',
+          loader: "esbuild-loader",
           options: {
-            loader: 'tsx',  // Or 'ts' if you don't need tsx
-            target: 'es2015'
-          }
-        }
+            loader: "tsx", // Or 'ts' if you don't need tsx
+            target: "es2015",
+          },
+        },
       ],
     },
     plugins: [
       new ModuleFederationPlugin({
-        name: 'container',
+        name: "container",
         remotes: {
-          micro1: `micro1@${getRemoteEntryUrl('micro1')}`,
-          micro2: `micro2@${getRemoteEntryUrl('micro2')}`
+          micro1: `micro1@${getRemoteEntryUrl("micro1")}`,
+          micro2: `micro2@${getRemoteEntryUrl("micro2")}`,
         },
         shared: {
           react: { singleton: true },
-          'react-dom': { singleton: true },
-          'react-router-dom': { singleton: true },
-          'react-router-dom': { singleton: true }
+          "react-dom": { singleton: true },
+          "react-router-dom": { singleton: true },
+          "react-router-dom": { singleton: true },
         },
       }),
       new HtmlWebpackPlugin({
-        template: './public/index.html',
+        template: "./public/index.html",
       }),
+      new ForkTsCheckerWebpackPlugin(),
     ],
   }
 
   const webpackConfig = getWebpackConfigByMode(PRODUCTION_MODE)
 
-  return merge(commonConfig, webpackConfig);
-};
+  return merge(commonConfig, webpackConfig)
+}
